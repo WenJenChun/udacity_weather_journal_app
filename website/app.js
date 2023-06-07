a2483ea407bbc9650bd2ae8a92b15e9ab6951d46
WenJenChun <daydreamsss@hotmail.com>
2023-06-07 13:55:30 +0800

    commit

--- website/app.js
+++ website/app.js
@@ -6,7 +6,7 @@ const apiUrl = (zipCode) => {
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+ 1 + '.'+ d.getDate()+'.'+ d.getFullYear();
 
 document.getElementById('generate').addEventListener('click', performAction);
 
